42c5dfe0363a7368221e7aef785116a96aa849e8
Jacob Coby <jacob@mi-squared.com>
2021-09-28 10:18:31 -0400

    Implement JsonSerializable for Choice
    Fixes api bug

--- src/com/zoho/crm/api/util/Choice.php
+++ src/com/zoho/crm/api/util/Choice.php
@@ -1,13 +1,18 @@
 <?php
 namespace com\zoho\crm\api\util;
 
-class Choice
+class Choice implements \JsonSerializable
 {
     private $value;
 
     public function __construct($value)
     {
         $this->value = $value;
+    }
+
+    public function jsonSerialize()
+    {
+        return $this->value;
     }
 
     public function getValue()
